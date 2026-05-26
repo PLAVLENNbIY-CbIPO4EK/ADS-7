15759b2ed1023f8cf0c26c1ad07e265875006479
PLAVLENNbIY-CbIPO4EK <ilya.potor2713@gmail.com>
2026-05-26 20:07:18 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,67 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() {
+    first = nullptr;
+    countOp = 0;
+}
+
+void Train::addCar(bool light) {
+    Car* car = new Car;
+
+    car->light = light;
+    car->n = nullptr;
+    car->p = nullptr;
+
+    if (first == nullptr) {
+        first = car;
+
+        first->n = first;
+        first->p = first;
+    } else {
+        Car* tail = first->p;
+
+        tail->n = car;
+
+        car->p = tail;
+        car->n = first;
+
+        first->p = car;
+    }
+}
+
+int Train::getLength() {
+    countOp = 0;
+
+    if (first == nullptr) {
+        return 0;
+    }
+
+    int len = 1;
+
+    bool allOn = first->light;
+
+    const Car* cur = first->n;
+
+    while (cur != first) {
+        len++;
+
+        if (!cur->light) {
+            allOn = false;
+        }
+
+        cur = cur->next;
+    }
+
+    if (allOn) {
+        countOp = len * (len + 1);
+    } else {
+        countOp = len * 2;
+    }
+
+    return len;
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
